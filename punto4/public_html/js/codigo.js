f60b77c387787e8b91f4f3ff1e5ee49efecaf926
DANIEL <DANIEL@DANIEL-HP>
2017-03-22 12:03:31 -0500

    falta terminar

--- punto4/public_html/js/codigo.js
+++ punto4/public_html/js/codigo.js
@@ -1,7 +1,86 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function(){
+    var a=0;
+    var b=0;
+    
+    
+    
+        //alert("Bienvenido"); 
+    $("#for1").submit(function (parametro){
+        parametro.preventDefault();
+        
+        if(d === e){alert("Error un equipo no puede jugar contra si mismo ");}
+        
+        var d="";
+        var e="";
+        
+        var pj=0;
+        var pg=0;
+        var pp=0;
+        var pe=0;
+        var gf=0;
+        var gc=0;
+        var pun=0;
+        
+        a= parseInt($("#txtL").val());
+        b= parseInt($("#txtV").val());
+        
+        d=$("#listaL").val();
+        e=$("#listaV").val();
+        
+        pj= parseInt($("#pja").val());
+        pg= parseInt($("#pga" ).val());
+        pp= parseInt($("#ppa").val());
+        pe= parseInt($("#pea").val());
+        gf= parseInt($("#gfa").val());
+        gc= parseInt($("#gca").val());
+        pun= parseInt($("#puna").val());
+       
+        if(d !== e){  
+            if(a > b)
+            {
+                
+                
+                
+                
+                
+            }
+            
+            
+            //$("#divres").append("<tr>");
+            $("#divres").append("<td>" + a + "+" + b + " = </td>");
+            $("#divres").append("<td>" + c + "</td></tr>");
+            //$("#divres").append(c);
+        }
+    
+             
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
 
 
